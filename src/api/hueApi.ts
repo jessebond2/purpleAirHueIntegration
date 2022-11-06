90037a435724a21a5d08e277d247203c79f3e32a
jessebond2 <30394856+jessebond2@users.noreply.github.com>
2022-11-06 00:08:53 -0700

    Setting up basic control

--- src/api/hueApi.ts
+++ src/api/hueApi.ts
@@ -1,8 +1,52 @@
-import config from "../config/config.json";
-
-const { hueBridgeIp } = config;
+import axios, { AxiosInstance } from 'axios'
+import https from 'https'
+import { hueBridgeIp } from '../config/config.json'
 
 export const HUE_ROUTES = {
   DEBUG: `/debug/clip.html`,
-  
-};
+  CREATE_USER: '/api',
+  LIGHTS: '/api/:userId/lights',
+  LIGHT: '/api/:userId/lights/:lightId',
+  LIGHT_STATE: '/api/:userId/lights/:lightId/state',
+}
+
+interface CreateUserOptions {
+  devicetype: string
+}
+
+interface UpdateLightOptions {
+  on?: boolean
+  sat?: number
+  bri?: number
+  hue?: number
+}
+
+export class HueApi {
+  private api: AxiosInstance
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: `https://${hueBridgeIp}`,
+      httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    })
+  }
+
+  createUser(data?: CreateUserOptions) {
+    return this.api.post(HUE_ROUTES.CREATE_USER, data)
+  }
+
+  getLights(userId: string) {
+    const route = HUE_ROUTES.LIGHTS.replace(':userId', userId)
+    return this.api.get(route)
+  }
+
+  getLight(userId: string, lightId: string) {
+    const route = HUE_ROUTES.LIGHTS.replace(':userId', userId).replace(':lightId', lightId)
+    return this.api.get(route)
+  }
+
+  updateLight(userId: string, lightId: string, data?: UpdateLightOptions) {
+    const route = HUE_ROUTES.LIGHT_STATE.replace(':userId', userId).replace(':lightId', lightId)
+    return this.api.put(route, data)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
+import * as dotenv from 'dotenv'
 import config from './config/config.json'
+import { HueApi } from './api/hueApi'
 
-console.log('hello wurld', config)
+dotenv.config()
+
+const hueApi = new HueApi()
+const userId = process.env.HUE_API_KEY || null
+
+async function main() {
+  try {
+    if (userId === null) {
+      const result = await hueApi.createUser({ devicetype: 'purpleAirApp' })
+      console.log(result.data)
+      return
+    }
+
+    const lights: Record<string, Record<string, string>> = (await hueApi.getLights(userId)).data
+    //console.log(lights)
+
+    Object.entries(lights).forEach(async ([id, data]) => {
+      console.log(`Updating light ${data.name}`)
+      try {
+        const res = await hueApi.updateLight(userId, id, { on: true, sat: 254, bri: 254, hue: 10000 })
+        console.log(res.status, res.data)
+      } catch (e) {
+        console.log(`Light id ${id} doesn't exist`)
+      }
+    })
+  } catch (e) {
+    const error = e as Error
+    console.log('Error:', error.message)
+  }
+}
+
+;(async () => main())()
